2a4014a0265143a285180109cf3d75211226cdd9
DamneD <alihan93.93@gmail.com>
2018-02-20 00:41:18 +0300

    Added sockets

--- app.js
+++ app.js
@@ -5,14 +5,38 @@ var Gpio = require('onoff').Gpio,
 	lastChangeTime = null;
 
 button.watch((err, val) => {
-	if ((Date.now() - lastChangeTime) > 400) {
+	if ((Date.now() - lastChangeTime) > 400 && val == 1) {
 		triggerRelay();
 	}
 
 	lastChangeTime = Date.now();
 });
 
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 function triggerRelay() {
 	triggerEnabled = triggerEnabled == 1 ? 0 : 1;
 	relay1.writeSync(triggerEnabled);
+
+	io.emit('relay.state', triggerEnabled);
 }
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+	console.log('A user connected');
+
+	socket.emit('relay.state', triggerEnabled);
+
+	socket.on('relay.trigger', function() {
+		triggerRelay();
+	});
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
